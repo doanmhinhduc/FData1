9ec0c8db6d35e5f52de9795f7325096f177ad5c2
doanmhinhduc <70931541+doanmhinhduc@users.noreply.github.com>
2022-04-13 10:44:31 +0700

    ds

--- src/main/java/com/example/fdata/model/tbUserModel.java
+++ src/main/java/com/example/fdata/model/tbUserModel.java
@@ -1,25 +1,29 @@
-import com.example.fdata.entity.tbUser;
-import com.example.hellot2008m.util.ConnectionHelper;
-import com.example.hellot2008m.util.SQLConfig;
+package com.example.fdata.model;
 
-import java.sql.*;
+import com.example.fdata.entity.tbUser;
+import com.example.fdata.util.ConnectionHelper;
+import com.example.fdata.util.SQLConfig;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
-public class AccountModel {
-    public boolean save(Account account) {
+public class tbUserModel {
+    public boolean save(tbUser tbUser) {
         try {
             Connection connection = ConnectionHelper.getConnection();
             PreparedStatement preparedStatement =
                     connection.prepareStatement(
                             SQLConfig.INSERT_ACCOUNT);
-            preparedStatement.setString(1, account.getUsername());
-            preparedStatement.setString(2, account.getFullName());
-            preparedStatement.setString(3, account.getPasswordHash());
-            preparedStatement.setString(4, account.getSalt());
-            preparedStatement.setString(5, account.getCreatedAt());
-            preparedStatement.setInt(6, account.getStatus());
+            preparedStatement.setString(1, tbUser.getUsername());
+            preparedStatement.setString(2, tbUser.getFullName());
+            preparedStatement.setString(3, tbUser.getPasswordHash());
+            preparedStatement.setString(4, tbUser.getSalt());
+            preparedStatement.setString(5, tbUser.getCreatedAt());
             // PrepareStatement
             preparedStatement.execute();
             return true;
@@ -29,7 +33,7 @@ public class AccountModel {
         return false;
     }
 
-    public Account findAccountByUsername(String username) {
+    public tbUser findAccountByUsername(String username) {
         try {
             Connection connection = ConnectionHelper.getConnection();
             PreparedStatement preparedStatement =
@@ -42,24 +46,23 @@ public class AccountModel {
                 String usernameDatabase = resultSet.getString("username");
                 String passwordHash = resultSet.getString("passwordHash");
                 String salt = resultSet.getString("salt");
-                Account account = new Account();
-                account.setUsername(usernameDatabase);
-                account.setPasswordHash(passwordHash);
-                account.setSalt(salt)
+                tbUser account = new tbUser();
+                tbUser.setUsername(usernameDatabase);
+                tbUser.setPasswordHash(passwordHash);
+                tbUser.setSalt(salt)
                 ;
-                account.setFullName(resultSet.getString("fullName"));
-                account.setStatus(resultSet.getInt("status"));
-                account.setFailureCount(resultSet.getInt("failureCount"));
-                account.setCreatedAt(resultSet.getString("createAt"));
+                tbUser.setFullName(resultSet.getString("fullName"));
+                tbUser.setFailureCount(resultSet.getInt("failureCount"));
+                tbUser.setCreatedAt(resultSet.getString("createAt"));
                 try {
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
                     if (resultSet.getString("lockTime") != null) {
-                        account.setLockTime(LocalDateTime.parse(resultSet.getString("lockTime"), formatter));
+                        tbUser.setLockTime(LocalDateTime.parse(resultSet.getString("lockTime"), formatter));
                     }
                 } catch (DateTimeParseException ex) {
                     System.out.println(ex.getMessage());
                 }
-                return account;
+                return tbUser;
             }
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -67,15 +70,15 @@ public class AccountModel {
         return null;
     }
 
-    public boolean updateLock(String username, Account account) {
+    public boolean updateLock(String username, tbUser tbUser) {
         try {
             Connection connection = ConnectionHelper.getConnection();
             PreparedStatement statement =
                     connection.prepareStatement(
                             SQLConfig.LOCK_ACCOUNT);
-            statement.setInt(1, account.getStatus());
-            statement.setInt(2, account.getFailureCount());
-            statement.setString(3, account.getLockTime().toString());
+            statement.setInt(1, tbUser.getStatus());
+            statement.setInt(2, tbUser.getFailureCount());
+            statement.setString(3, tbUser.getLockTime().toString());
             statement.setString(4, username);
             statement.execute();
             return true;
